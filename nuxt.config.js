418b481d02f71872cb640c7fb11305ea7fe0b470
Victor Jansen <victorfjansen@gmail.com>
2021-09-17 16:11:28 -0300

    acertando as coisas para o gitpages

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/portfolio/',
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
